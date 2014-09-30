6d4f10e15c75b41a8f30b856a91a7c56f1f40be5
Re'em Bensimhon <reem@forter.com>
2014-09-30 10:12:05 +0300

    Removed un-needed Tuple serial ID mechanism

--- src/main/java/com/forter/storm/apis/wrappers/ApisMorphedTuple.java
+++ src/main/java/com/forter/storm/apis/wrappers/ApisMorphedTuple.java
@@ -12,22 +12,11 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Extending the base tuple impl in order to supply own equality
  */
 public class ApisMorphedTuple extends TupleImpl {
-    public static AtomicInteger serialGenerator = new AtomicInteger(0);
-
-    private final int serial;
-
     public ApisMorphedTuple(Tuple input, List<Object> passThroughParams, GeneralTopologyContext context) {
         this(input, passThroughParams, context, input.getSourceTask());
     }
 
     public ApisMorphedTuple(Tuple input, List<Object> passThroughParams, GeneralTopologyContext context, Integer taskId) {
         super(context, passThroughParams, taskId, Utils.DEFAULT_STREAM_ID, input.getMessageId());
-        this.serial = serialGenerator.getAndAdd(1);
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other instanceof ApisMorphedTuple &&
-                ((ApisMorphedTuple) other).serial == this.serial;
     }
 }
